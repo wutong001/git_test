464a114f14b1e3c7657507c8dba6f62b70c233c3
wutong001 <61954944+wutong001@users.noreply.github.com>
2021-08-03 19:33:14 +0800

    pull commit3

--- src/main/java/com/wutong/git/GitTest.java
+++ src/main/java/com/wutong/git/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("push test");
         System.out.println("pull test");
          System.out.println("pull test2");
+         System.out.println("pull test3");
     }
 }
