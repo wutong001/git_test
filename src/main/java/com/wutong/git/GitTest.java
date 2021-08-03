004358a1bb240a13693b6630898822606163feb6
wutong <2983636772@qq.com>
2021-08-03 17:56:49 +0800

    first commit

---
+++ src/main/java/com/wutong/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.wutong.git;
+
+/**
+ * @author wutong
+ * @version V1.0
+ * @date 2021/8/3 下午5:54
+ * @Copyright
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
