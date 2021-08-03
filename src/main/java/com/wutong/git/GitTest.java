9eb5ef77644e87153106e4dd7255a3232f9775a3
wutong <2983636772@qq.com>
2021-08-03 19:01:29 +0800

    master commit2

--- src/main/java/com/wutong/git/GitTest.java
+++ src/main/java/com/wutong/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("master test");
     }
 }
